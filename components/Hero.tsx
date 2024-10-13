3864ae0c467b2f3f2583ec203c5326b3690bc225
Stephen Ochieng <materiamystic@gmail.com>
2024-10-13 22:56:20 +0300

    Initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import { MagicButton } from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+
+function Hero() {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-top-20 md:-left-32 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full md:top-20 h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="h-screen w-full bg-black-100 bg-grid-white/[0.05] flex items-center justify-center absolute top-0 left-0">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="text-blue-100 uppercase tracking-widest text-xs text-center max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-white">
+            Hi, I&apos;m Stephen, a Fullstack Developer based in Kenya.
+          </p>
+          <a href="#about" title="My work">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6e069f375b0de0560af09b585721aa50@o4508106141401088.ingest.us.sentry.io/4508106152804352",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
